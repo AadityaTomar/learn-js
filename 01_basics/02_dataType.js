81514abeb43fcaec3892fe1781f4cd96923e0ae8
AadityaTomar <121477552+AadityaTomar@users.noreply.github.com>
2023-06-04 09:53:35 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS Code as newer version
+
+//alert(3 + 3)  we are using nodejs, not brower
+
+console.log(3 + 3)   // code readability should be high
+
+console.log("Aditya Tomar")
+
+let name ="aditya"
+let age = 22
+let isLoggedIn = true;
+
+let state = null
+
+// number  => 2 to power 53 
+// bigint
+// string 
+// boolean => true/false
+// null => standalone value
+//undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof "aditya")
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
