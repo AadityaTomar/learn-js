e058b4ca274bff9587531b662efe73d8e653adde
AadityaTomar <121477552+AadityaTomar@users.noreply.github.com>
2023-06-06 03:56:28 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,17 +11,51 @@ let score = "adityatomar"
 // "33abc" => NaN
 // true => 1; fasle => 0
 
-let isLoggedIn = "adityaTomar"
+// let isLoggedIn = "adityaTomar"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false
 // " " => false
 // "aditya" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************************Operations ********************************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+// console.log("")
+let num1,num2 , num3
+num1 = num2 = num3 = +2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
