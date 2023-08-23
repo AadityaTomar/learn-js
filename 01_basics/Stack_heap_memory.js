dae360ba764d71580a428ffd41a4f0796cfb65e3
AadityaTomar <121477552+AadityaTomar@users.noreply.github.com>
2023-08-23 07:23:41 +0000

    Stack and heap

---
+++ 01_basics/Stack_heap_memory.js
@@ -0,0 +1,27 @@
+console.log("hare krishna");
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom";
+
+let anothername = myYoutubename;
+
+anothername = "chaiaurcode";
+
+
+console.log(anothername);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "adi@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
