8e9651b3c3a2a298e8487a88455f03afe1397bc2
AadityaTomar <121477552+AadityaTomar@users.noreply.github.com>
2023-09-02 05:09:25 +0000

    ifelse

---
+++ 04_control_flow/one.js
@@ -0,0 +1,64 @@
+// if 
+// const isUserloggedIn = true;
+const temperature = 41;
+
+
+// if(temperature < 40) {
+//    console.log("less than  50");
+// }
+
+// else {
+//     console.log("temperature is greater than 50 ");
+// }
+
+// console.log("Execute");
+
+// < , > ,<= , >=, ==, !=, === , !== 
+
+
+// const score = 200;
+
+// if(score > 100) {
+//   const  power = "fly"
+//   console.log(`User power : ${power}`);  
+
+// }
+
+// console.log(`User power : ${power}`);  
+
+const balance = 1000;
+
+//if(balance >= 500) console.log("test"), console.log("text2");
+
+
+// if(balance < 500) {
+//   console.log("less than ");
+// }
+
+// else if(balance < 750)
+// {
+//     console.log("less than 750");
+// }
+
+// else if (balance < 900) {
+//    console.log(balance < 900)
+// }
+
+// else {
+//     console.log("less than 1200");
+// }
+
+
+const isUserloggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(isUserloggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course ");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
+
