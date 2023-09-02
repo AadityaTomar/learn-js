acd8574a298a0f1cfab9ba3f7bf60214bd550073
AadityaTomar <121477552+AadityaTomar@users.noreply.github.com>
2023-09-02 06:39:38 +0000

    switchStatement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "fab":
+        console.log("fab");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break; 
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
