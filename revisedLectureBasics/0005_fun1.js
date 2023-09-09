74e9e9e975d2c62bca68036b895b9ff4d91cf714
AadityaTomar <121477552+AadityaTomar@users.noreply.github.com>
2023-09-09 04:20:47 +0000

    functions is call

--- revisedLectureBasics/0005_fun1.js
+++ revisedLectureBasics/0005_fun1.js
@@ -36,4 +36,35 @@ function loginUserMessage(username = "sam") {
 }
 
 // console.log(loginUserMessage("Aditya"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCardPrice(val1, val2,...num1) {
+   return num1
+}
+
+// console.log(calculateCardPrice(200,400,500,9999))
+
+const user = {
+    username: "aditya",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price} `);
+}
+
+// handleObject(user)
+handleObject({
+    username:"adi",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
